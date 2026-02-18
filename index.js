2f2725c4f6f83554ac0930e8b9f3172568168afc
jeshikashrestha <jeshikashrestha25@gmail.com>
2026-02-18 14:31:26 +0545

    env file configuration

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+
 const app  = express();
  
-const  PORT = 5000;
+const  PORT = process.env.PORT = 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
